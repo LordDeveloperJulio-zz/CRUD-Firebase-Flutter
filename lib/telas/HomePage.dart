6aa37305bf1c7ac7ce0d4c8b3cd774c9c901e3de
Julio Gabriel <dev.julio.furtado@gmail.com>
2020-03-20 20:27:43 -0400

    Listando postagem

--- lib/telas/HomePage.dart
+++ lib/telas/HomePage.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crudfirebase/models/Post.dart';
 import 'package:crudfirebase/widgets/HomeDrawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,7 @@ import 'package:provider/provider.dart';
 
 import 'LoginPage.dart';
 import 'PostsNewPage.dart';
+import 'PostsShowPage.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -23,10 +26,41 @@ class _HomePageState extends State<HomePage> {
         title: Text("Pagina inicial"),
       ),
       drawer: HomeDrawer(),
-      body: Center(
-          child: isAuthenticated
-              ? Text('Página inicial após o login')
-              : Text('Página inicial antes do login')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collectionGroup('posts')
+            .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Carregando...');
+            default:
+              return ListView(
+                children: snapshot.data.documents.map((DocumentSnapshot document) {
+                  final post = Post.fromFirestore(document);
+                  return ListTile(
+                    title: Text(
+                      post.title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(post.content),
+//                    onTap: () {
+//                      // https://flutter.dev/docs/cookbook/navigation/passing-data#4-navigate-and-pass-data-to-the-detail-screen
+//                      Navigator.push(
+//                        context,
+//                        MaterialPageRoute(
+//                          builder: (context) => PostsShowPage(post: post),
+//                        ),
+//                      );
+//                    },
+                  );
+                }).toList(),
+              );
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if (isAuthenticated) {
@@ -41,7 +75,7 @@ class _HomePageState extends State<HomePage> {
             );
           }
         },
-        tooltip: 'New Post',
+        tooltip: 'Nova Postagem',
         child: Icon(Icons.note_add),
       ),
     );
