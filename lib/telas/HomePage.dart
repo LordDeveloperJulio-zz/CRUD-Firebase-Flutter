5acba77aeb2eb775cda51dc37c9912ff5f03a1b9
Julio Gabriel <dev.julio.furtado@gmail.com>
2020-03-20 20:06:10 -0400

    Criando postagem

--- lib/telas/HomePage.dart
+++ lib/telas/HomePage.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'LoginPage.dart';
+import 'PostsNewPage.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -24,6 +27,23 @@ class _HomePageState extends State<HomePage> {
           child: isAuthenticated
               ? Text('Página inicial após o login')
               : Text('Página inicial antes do login')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (isAuthenticated) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PostsNewPage()),
+            );
+          } else {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          }
+        },
+        tooltip: 'New Post',
+        child: Icon(Icons.note_add),
+      ),
     );
   }
 }
