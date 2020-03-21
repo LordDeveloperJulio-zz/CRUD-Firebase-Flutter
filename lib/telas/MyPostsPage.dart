f8636a740af37752ba930fdbaaa2ca86f17dbb3e
Julio Gabriel <dev.julio.furtado@gmail.com>
2020-03-20 20:52:00 -0400

    Excluindo postagem

--- lib/telas/MyPostsPage.dart
+++ lib/telas/MyPostsPage.dart
@@ -1,21 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crudfirebase/models/Post.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class MyPostsPage extends StatefulWidget {
+import 'PostsShowPage.dart';
 
-  @override
-  _MyPostsPageState createState() => _MyPostsPageState();
-}
-
-class _MyPostsPageState extends State<MyPostsPage> {
-
+class MyPostsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<FirebaseUser>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("MyPosts Page"),
+        title: Text('Minhas Postagens'),
       ),
-      body: Center(
-          child: Text('This is myposts page')
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collection("users")
+            .document(user.uid)
+            .collection("posts")
+            .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Carregando...');
+            default:
+              return ListView(
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  final post = Post.fromFirestore(document);
+
+                  return ListTile(
+                    title: Text(
+                      post.title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(post.content),
+                    trailing: PopupMenuButton(
+                      onSelected: (result) async {
+                        final type = result["type"];
+                        final post = result["value"];
+                        switch (type) {
+                          case 'edit':
+                            print('clique em editar');
+                            break;
+                          case 'delete':
+                            await Firestore.instance
+                                .collection('users')
+                                .document(user.uid)
+                                .collection('posts')
+                                .document(post.id)
+                                .delete();
+                            break;
+                        }
+                      },
+                      itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                        PopupMenuItem(
+                          value: {"type": "edit", "value": post},
+                          child: Text('Editar'),
+                        ),
+                        PopupMenuItem(
+                          value: {"type": "delete", "value": post},
+                          child: Text('Excluir'),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PostsShowPage(post: post),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              );
+          }
+        },
       ),
     );
   }
